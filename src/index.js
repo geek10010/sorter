5d6468ad2a26bfb7d99f5d733045ef81e25759b6
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-02-25 22:37:44 +0300

    src/index.js implement class Sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr = [];
+    this.compareFunction = (a, b) => a - b;
   }
-
+  
   add(element) {
-    // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+      let tempArr = [];
+      for (let i = 0; i < indices.length; i++) {
+          tempArr.push(this.arr[indices[i]])
+      }
+      tempArr.sort(this.compareFunction);
+      indices.sort();
+      for (let i = 0; i < indices.length; i++) {
+          this.arr[indices[i]] = tempArr[i];
+      }
+      return this.arr;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
